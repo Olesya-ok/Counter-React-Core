43f2f0c88cd0a008ce62ccac45e2cfe90429bcf0
Olesya-ok <malyshalesya89@gmail.com>
2025-02-02 19:35:02 +0200

    add 02.02.2025

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";  // Обратите внимание на новый импорт
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store";
